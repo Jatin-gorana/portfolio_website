e9665e440257c21b0039acb3067d901aa73d9023
Jatin_Gorana <jatingorana123@gmail.com>
2024-01-28 14:38:50 +0530

    final changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,32 +13,38 @@ const Portfolio = () => {
         {
             id: 1,
             src: arrayDestruct,
-            name: "Mobile Analysis"
+            name: "Mobile Analysis",
+            href: "https://github.com/Jatin-gorana/Mobile_analysis_using_mysqlandpowerbi/tree/main",
         },
         {
             id: 2,
             src: installNode,
             name: "Todo List",
+            href: "https://github.com/Jatin-gorana/todo_list/tree/main",
         },
         {
             id: 3,
             src: navbar,
             name: "X-O game",
+            href: "https://github.com/Jatin-gorana/Python_projects/blob/main/XO_GAME.ipynb",
         },
         {
             id: 4,
             src: reactParallax,
             name: "SMTP Library",
+            href: "https://github.com/Jatin-gorana/Pandas_Udemy/blob/main/E-mail%20Project.py",
         },
         {
             id: 5,
             src: reactSmooth,
             name: "Web Scrapping",
+            href: "https://github.com/Jatin-gorana/Pandas_Udemy/blob/main/WebScrapping%20and%20Data%20Analysis.ipynb",
         },
         {
             id: 6,
             src: reactWeather,
             name: "Text to Speech",
+            href: "https://github.com/Jatin-gorana/Python_projects/blob/main/main.py",
         },
     ]
 
@@ -52,13 +58,13 @@ const Portfolio = () => {
 
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
 
-            {portfolio.map(({id,src,name})=> (
+            {portfolio.map(({id,src,name,href})=> (
                 
                 <div key={id} className="shadow-md shadow-gray-800 rounded-lg">
                     <img src={src} alt="" className="rounded-md duration-200 hover:scale-105"/>
                     <div className="flex items-center justify-center">
                         {/* <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button> */}
-                        <button className="px-6 py-3 m-4 duration-200 hover:scale-105">{name}</button>
+                        <button className="px-6 py-3 m-4 duration-200 hover:scale-105"><a href={href} target='_blank' rel='noreferrer'>{name}</a></button>
                     </div>
                 </div>
             ))}
